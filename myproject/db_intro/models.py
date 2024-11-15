dbca76175753b04ab3b9ad2095aaedee564aecf0
Sebastian Gaus <Sebastian.Gaus@enders-training.de>
2024-11-15 12:25:40 +0100

    ManytoMany and OnetoOne Relationships

--- myproject/db_intro/models.py
+++ myproject/db_intro/models.py
@@ -24,3 +24,42 @@ class Book(models.Model):
     
     def __str__(self):
         return f'{self.title} von {self.author}'
+
+
+# ManytoMany Relationship
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return f'Course: {self.title} Students: {self.students}'
+    
+# ZwischenModell für ManytoMany Relationships erstellen
+class Membership(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enroll_date = models.DateField()
+    
+    def __str__(self):
+        return f'{self.student.name}'
+
+
+# OnetoOne Relationship
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.username
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=100, default='info@hiphop.net')
+    
+    def __str__(self) -> str:
+        return self.user.username
