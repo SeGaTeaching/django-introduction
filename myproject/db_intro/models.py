c05d8ed302d6686fac00ae51944d76e53e9aa4fd
Sebastian Gaus <Sebastian.Gaus@enders-training.de>
2024-11-15 09:56:05 +0100

    weitere Models angelegt to show Many-to-one Relationship

--- myproject/db_intro/models.py
+++ myproject/db_intro/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    user_email = models.EmailField(default='user@example.com')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+
+# ForeignKey (Many-to-one Relationship)
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.title} von {self.author}'
